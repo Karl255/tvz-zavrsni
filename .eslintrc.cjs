35b9b20b9f103aeb54578b8e1e7ef879d9c0c242
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-02-07 19:33:20 +0100

    linting setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,37 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["svelte", "@typescript-eslint", "prettier"],
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: [".svelte"],
 	},
 	env: {
 		browser: true,
-		es2017: true,
-		node: true
+		es2022: true,
+		node: true,
 	},
 	overrides: [
 		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
+			files: ["*.svelte"],
+			processor: "svelte/svelte",
+			parser: "svelte-eslint-parser",
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
+	rules: {
+		"prettier/prettier": ["warn"],
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{
+				varsIgnorePattern: "^_",
+				argsIgnorePattern: "^_",
+			},
+		],
+	},
 };
