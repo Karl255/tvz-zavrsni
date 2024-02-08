cf6816d04157d4a651136009ec06d3cf737e26b5
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-02-08 16:25:26 +0100

    #3 dev page

---
+++ src/routes/dev/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	function initDb() {
+		// no tables exist rn
+	}
+
+	function seedDb() {
+		// no tables exist rn
+	}
+</script>
+
+<h1>Development stuff</h1>
+
+<section>
+	<h2>Actions</h2>
+
+	<ul>
+		<li><button on:click={initDb}>Initialize DB</button></li>
+		<li><button on:click={seedDb}>Seed DB</button></li>
+	</ul>
+</section>
